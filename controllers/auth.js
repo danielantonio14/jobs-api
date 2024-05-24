fe6779ac3f5a94c4c7b7d3d5e1109425ed9d392c
danielantonio14 <63520365+danielantonio14@users.noreply.github.com>
2024-05-24 22:13:45 +0100

    main jobs endpoints

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,45 @@
-const register = async(req,res)=> {
-    res.send('register user');
+const User = require('../models/User');
+const { StatusCodes } = require('http-status-codes');
+const { BadRequestError, UnauthorizedError } = require('../errors');
+const jwt = require('jsonwebtoken')
+
+const register = async (req, res) => {
+    const { name, email, password } = req.body;
+
+    // Validação básica
+    if (!name || !email || !password) {
+        throw new BadRequestError('Por favor, forneça nome, email e senha.');
+    }
+
+    // Criação do usuário
+    const user = await User.create({ name, email, password });
+    const token = jwt.sign({userId: user.id, name: user.name}, 'jwtSecret',{expiresIn: '30d',
+    })
+    res.status(StatusCodes.CREATED).json({ user:{name:user.name}, token });
 }
 
-const login = async(req,res)=> {
-    res.send('login user');
+const login = async (req, res) => {
+    const { email, password } = req.body;
+
+    // Validação básica
+    if (!email || !password) {
+        throw new BadRequestError('Por favor, forneça email e senha.');
+    }
+
+    // Verificação do usuário
+    const user = await User.findOne({ email });
+    if (!user) {
+        throw new UnauthorizedError('Credenciais inválidas.');
+    }
+
+    // Comparação de senha
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (!isPasswordCorrect) {
+        throw new UnauthorizedError('Credenciais inválidas.');
+    }
+
+    // Retornar resposta de sucesso (aqui você poderia gerar um token JWT, por exemplo)
+    res.status(StatusCodes.OK).json({ user });
 }
 
 module.exports = {
--- db/connect.js
+++ db/connect.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const connectDB = (url) => {
   return mongoose.connect(url, {
@@ -7,6 +7,12 @@ const connectDB = (url) => {
     useFindAndModify: false,
     useUnifiedTopology: true,
   })
-}
+      .then(() => {
+        console.log('MongoDB connected successfully');
+      })
+      .catch((error) => {
+        console.error('MongoDB connection failed:', error);
+      });
+};
 
-module.exports = connectDB
+module.exports = connectDB;
